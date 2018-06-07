363e75b0a723aea98fc8eb4fab80c7bce885a4ef
kmvi <kmvi@users.noreply.github.com>
2018-06-07 11:46:19 +0300

    read input file

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,14 @@
+import * as fs from 'fs';
+import * as util from 'util';
+
 if (process.argv.length < 3) {
     console.log('Usage: javascript-deobfuscator <file>');
     process.exit(1);
 }
 
+async function main() {
+    const readFile = util.promisify(fs.readFile);
+    var code = await readFile(process.argv[2], 'utf8');
+}
+
+main().catch(console.error);
